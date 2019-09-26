50bf1806278ec5526ad775fbc03bfee579879c05
Oscar Calzada <oscarcalzada@oscars-air.lan>
2019-09-25 23:13:30 -0600

    Commit changes

--- pages/predictions.py
+++ pages/predictions.py
@@ -3,7 +3,6 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import matplotlib.pyplot as plt
 from fbprophet import Prophet
 import numpy as np 
 import pandas as pd
